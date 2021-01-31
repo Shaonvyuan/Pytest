fa43448621c864db9c61edffb75ef8cbb6e5bde9
Shaonvyuan <melodyer@163.com>
2021-01-31 13:02:03 +0800

    Add test_add()

--- testing/test_calculator.py
+++ testing/test_calculator.py
@@ -14,7 +14,8 @@ from pythoncode.Calculator import Calculator
 def get_datas():
     with open("./data/calc.yml") as f:
         datas = yaml.safe_load(f)
-    return (datas['add']['datas'], datas['add']['ids'])
+    # return (datas['add']['datas'], datas['add']['ids'])
+    return datas
 
 
 # yaml json excel csv xml
@@ -31,21 +32,20 @@ class TestCalc:
     def teardown_class(self):
         print("结束计算")
 
-    @pytest.mark.parametrize("a, b, result", datas[0], ids=datas[1])
-    def test_add(self, a, b, result):
-        print(f"a={a} , b ={b} ,result={result}")
-        assert result == self.calc.add(a, b)
+    @pytest.mark.parametrize("a, b, expected", get_datas()['add']['datas'], ids=get_datas()['add']['ids'])
+    def test_add(self, a, b, expected):
+        result = self.calc.add(a, b)
+        assert expected == result
 
-    def test_add1(self):
-        datas = [[1, 1, 2], [100, 400, 500], [1, 0, 1]]
-        for data in datas:
-            print(data)
-            assert data[2] == self.calc.add(data[0], data[1])
+    # def test_add1(self):
+    #     datas = [[1, 1, 2], [100, 400, 500], [1, 0, 1]]
+    #     for data in datas:
+    #         print(data)
+    #         assert data[2] == self.calc.add(data[0], data[1])
 
-    # TODO: 完善相加功能
-    # TODO: 相除功能
-    def test_div(self):
-        datas = [[1, 1, 1], [100, 400, 500], [1, 0, 1]]
-        for data in datas:
-            print(data)
-            assert data[2] == self.calc.div(data[0], data[1])
+    # # TODO: 相除功能
+    # def test_div(self):
+    #     datas = [[1, 1, 1], [100, 400, 500], [1, 0, 1]]
+    #     for data in datas:
+    #         print(data)
+    #         assert data[2] == self.calc.div(data[0], data[1])
